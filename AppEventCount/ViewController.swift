648a72a4397692eac942966451aa24984cc3ca04
monashamsolebad <shamsolebad.mona@gmail.com>
2019-08-27 11:58:53 -0700

    done

--- AppEventCount/ViewController.swift
+++ AppEventCount/ViewController.swift
@@ -13,8 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
+    }
+     func updateUI(){
+        
+        didFinishLunchingLabel.text = "The app has launched \(launchCount) times"
+        willResignActiveLabel.text = "applicationWillResignActive has been called \(resignActiveCount) times"
+        didEnterBackgroundLabel.text = "applicationDidEnterBackground has been called \(didEnterBackgroundCount) times"
+        willenterForgroundLabel.text = "applicationWillEnterForeground has been called \(willenterForgroundCount) times"
+        didBecomeActiveLabel.text = "applicationDidBecomeActive has been called \(didBecomeActiveCount) times"
+        willTerminateLabel.text = "applicationWillTerminate has been called \(willTerminateCount) times"
     }
 
 
+    @IBOutlet var didFinishLunchingLabel: UILabel!
+    
+    @IBOutlet var willResignActiveLabel: UILabel!
+    
+    @IBOutlet var didEnterBackgroundLabel: UILabel!
+    
+    @IBOutlet var willenterForgroundLabel: UILabel!
+    
+    @IBOutlet var didBecomeActiveLabel: UILabel!
+    
+    @IBOutlet var willTerminateLabel: UILabel!
+    
+    var launchCount = 0
+    var resignActiveCount = 0
+    var didEnterBackgroundCount = 0
+    var willenterForgroundCount = 0
+    var didBecomeActiveCount = 0
+    var willTerminateCount = 0
+    
+    
+    
 }
 
